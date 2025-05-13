a03040aab8a0055290316d8d2bd8e6af2a4a5174
calebyhan <calebhantech@gmail.com>
2025-05-13 18:35:42 -0400

    login and cookies

--- kudos/src/App.js
+++ kudos/src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useCookies } from 'react-cookie'
 
 import Signup from "./components/Signup";
 import Home from "./components/Home";
 import Login from "./components/Login"
+import PostInterface from "./components/PostInterface";
 
 
 function App() {
+  const [cookies, setCookie] = useCookies(['user'])
+
+  function handleLogin(user) {
+    setCookie('user', user, { path: '/' })
+  }
+
   return (
     <Router>
       <Routes>
@@ -15,9 +22,14 @@ function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
   
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<Signup handleLogin={handleLogin} />} />
+          <Route path="/post" element={<PostInterface />} />
 
-      </Routes>
-    </Router>
+        </Routes>
+      </Router>
   );
 }
 
