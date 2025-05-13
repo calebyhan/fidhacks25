1506ac9033cedf9030d9eaa892b2dc7129e97ce4
izzjuliana <izzyjuliana1@gmail.com>
2025-05-13 15:28:33 -0400

    signup

---
+++ kudos/src/components/Home.js
@@ -0,0 +1,23 @@
+import React from "react"
+import "./Home.css"
+import { Link } from "react-router-dom"
+
+
+export default function Home() {
+    return (
+        <div className="Home">
+            <header className="Home title">Kudos</header>
+            <div className="Home slogan">slogan here</div>
+            <div className="elements">
+                <div className="submit-container">
+                <div className="submit">Login!</div>
+            <   div className="submit">
+                    <Link to="/signup">
+                        Sign Up!
+                    </Link>
+                </div>
+              </div>
+            </div>
+        </div>
+    )
+}
