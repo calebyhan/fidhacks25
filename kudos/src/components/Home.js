bcd56a814fc763888f5fe4923b521fa34a7ab34a
calebyhan <calebhantech@gmail.com>
2025-05-13 20:40:38 -0400

    home page pre login

--- kudos/src/components/Home.js
+++ kudos/src/components/Home.js
@@ -1,41 +1,44 @@
 import React from "react";
 import "./Home.css";
 import { Link, useNavigate } from "react-router-dom";
-import { useCookies } from "react-cookie"; // 👈 import useCookies
+import { useCookies } from "react-cookie";
 
 export default function Home() {
-    const [cookies, setCookie, removeCookie] = useCookies(['user']); // 👈 use react-cookie
+    const [cookies, setCookie, removeCookie] = useCookies(['user']);
     const navigate = useNavigate();
 
     const handleLogout = () => {
-        removeCookie('user', { path: '/' }); // 👈 cleanly remove cookie
+        removeCookie('user', { path: '/' });
         navigate("/");
     };
 
-    const isLoggedIn = !!cookies.user; // 👈 check if user cookie exists
+    const isLoggedIn = !!cookies.user;
 
     return (
         <div className="Home">
-            <header className="Home title">Kudos</header>
-            <div className="Home slogan">slogan here</div>
-            <div className="elements">
-                <div className="submit-container">
-                    {!isLoggedIn ? (
-                        <>
-                            <div className="submit">
-                                <Link to="/login">Login!</Link>
-                            </div>
-                            <div className="submit">
-                                <Link to="/signup">Sign Up!</Link>
-                            </div>
-                        </>
-                    ) : (
-                        <div className="submit">
-                            <Link to="/" onClick={ handleLogout }>Log Out</Link>
-                        </div>
-                    )}
+            {!isLoggedIn ? (
+            <>
+                <div className="logo">
+                <img src={require('./assets/logo.png')} alt="logo" />
                 </div>
-            </div>
+                <div className="elements">
+                <div className="submit-container">
+                    <div className="submit">
+                    <Link to="/login">LOGIN</Link>
+                    </div>
+                    <div className="submit">
+                    <Link to="/signup">SIGN UP</Link>
+                    </div>
+                </div>
+                </div>
+            </>
+            ) : (
+            <>
+                <div className="welcome">
+                <h2>Welcome back!</h2>
+                </div>
+            </>
+            )}
         </div>
     );
 }
